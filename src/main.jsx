3b2cdc4a80d945411de75bee573d2fa55fe113d5
Ch Lokesh <lokeshchirumamilla2104@gmail.com>
2025-06-08 01:34:17 +0530

    changed router to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import { HashRouter,Routes,Route} from 'react-router-dom'
 import Form from './components/Form.jsx'
 import Success from './pages/Success.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/success" element={<Success />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
